71756305218ebe0acdea2071520a672d935b82f0
turo-webdev <158546249+turo-webdev@users.noreply.github.com>
2026-02-19 22:40:59 -0600

    Configure Astro for custom domain deployment (Remove base path)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://turo-webdev.github.io',
-  base: '/Astro-tutorial',
+  site: 'https://artukperegrin.com',
   integrations: [sitemap()]
 });
